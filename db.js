137a396bcf423cb3a2876d733dcef1fb1113a48a
Chandukotoju <132841204+Chandukotoju@users.noreply.github.com>
2025-03-25 11:12:49 +0530

    changed from local db to server

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";  
 
-const MONGO_URI="mongodb://127.0.0.1:27017/social_app"
+const MONGO_URI="mongodb+srv://chandukotoju7:chandu200322@cluster0.yyz2r.mongodb.net/social_app?retryWrites=true&w=majority&appName=Cluster0"
 const connectDb=async()=>{
      try{
       await mongoose.connect(MONGO_URI); 
