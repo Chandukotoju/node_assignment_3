8e8685d18ae1200d007355625b4b2eb7ea5fa97e
Chandukotoju <132841204+Chandukotoju@users.noreply.github.com>
2025-03-25 20:55:39 +0530

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express"; 
 import connectDb from "./db.js"; 
-import router from "./routes.js";
+import router from "./routes.js"; 
+import cors from "cors";
 
 const app=express(); 
+app.use(cors())
 
 connectDb();
 
 app.use(express.json()) 
+
 
 app.use("/",router)
 
